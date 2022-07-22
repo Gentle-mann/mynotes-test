92d68acda01f658300118f82530d42c925cfba11
Ishaq Ibrahim <66851229+Gentle-mann@users.noreply.github.com>
2022-07-22 12:27:43 +0100

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,6 +19,7 @@ void main() {
     routes: {
       '/login/': (context) => const LoginView(),
       '/register/': (context) => const RegisterView(),
+      '/notes/' : (context) => const NotesView(),
     },
   ),
   );
@@ -36,17 +38,16 @@ class HomePage extends StatelessWidget {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
               final user = FirebaseAuth.instance.currentUser;
-              print(user);
+              devtools.log(user.toString());
               if (user != null){
                 if (user.emailVerified){
-                  print('Email is Verified');
+                  return const NotesView();
                 } else{
                   return const VerifyEmailView();
                 }
               } else{
                 return const LoginView();
               }
-                return const Text('Done');
             default:
               return const CircularProgressIndicator();
           }
@@ -55,6 +56,71 @@ class HomePage extends StatelessWidget {
   }
 }
 
+enum MenuAction { logout }
 
+class NotesView extends StatefulWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                        '/login/',
+                            (_) => false
+                    );
+                  }
+              }
+            },
+              itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                    value: MenuAction.logout,
+                    child: Text('Log out'),
+                ),
+              ];
+          },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+return showDialog<bool> (
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sign Out'),
+        content: const Text('Are you sure you want to sign out?'),
+        actions: [
+          TextButton(onPressed: () {
+            Navigator.of(context).pop(false);
+          }, child: const Text('Cancel'),
+          ),
+          TextButton(onPressed: () {
+            Navigator.of(context).pop(true);
+          }, child: const Text('Log out'),
+          ),
+        ],
+      );
+    }
+).then((value) => value ?? false);
+}
 
 
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -61,17 +63,17 @@ class _RegisterViewState extends State<RegisterView> {
                       email: email,
                       password: password,
                     );
-                    print(userCredentials);
+                    devtools.log(userCredentials.toString());
                   }
                   on FirebaseAuthException catch (e){
                     if (e.code== 'weak-password'){
-                      print('Weak Password');
+                      devtools.log('Weak Password');
                     }
                     else if(e.code == 'email-already-in-use'){
-                      print('Email is already in use by another user');
+                      devtools.log('Email is already in use by another user');
                     }
                     else if (e.code == 'invalid-email'){
-                      print('Invalid email entered');
+                      devtools.log('Invalid email entered');
                     }
                   }
                 },
